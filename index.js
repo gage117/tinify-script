afc41648214a57581d0aff8ede2e9254db249419
gage117 <57643501+gage117@users.noreply.github.com>
2021-02-01 13:38:29 -0600

    Improved readability

--- index.js
+++ index.js
@@ -16,30 +16,39 @@ const compressedFolder = './compressed';
         const files = await fs.promises.readdir(path.join(uncompressedFolder));
         // shift files to remove .gitignore file from start of array
         files.shift();
-        console.log("files to compress:", files, '\n');
-        let successful_compressions = 0;
+        console.log("files to compress:", files, '\n\n----------------');
+        let successfulCompressions = 0;
         for (let i = 0; i < files.length; i++) {
             try {
+                console.log(`> compressing ${files[i]}`)
                 const source = await tinify.fromFile(path.join(uncompressedFolder, files[i]));
-                // Get image dimensions to maintain aspect ratio while resizing
-                let dimensions = sizeOf(path.join(uncompressedFolder, files[i]));
-                console.log(`> image dimensions (${files[i]}):`, dimensions.width/2, dimensions.height/2);
+                
+                // Get current image dimensions to maintain aspect ratio while resizing
+                let originalDimensions = sizeOf(path.join(uncompressedFolder, files[i]));
+                let targetDimensions = {
+                    width: originalDimensions.width/2,
+                    height: originalDimensions.height/2
+                }
+                console.log(`> image dimensions:`, originalDimensions.width, originalDimensions.height);
+                console.log(`> target dimensions:`, targetDimensions.width, targetDimensions.height)
+                
                 // Resize to half of current resolution
                 const resized = await source.resize({
                     method: "fit",
-                    width: dimensions.width/2,
-                    height: dimensions.height/2
+                    width: targetDimensions.width,
+                    height: targetDimensions.height
                 });
+
                 // Save compressed image to compressed folder and log success
                 await resized.toFile(path.join(compressedFolder, files[i]));
-                console.log(`> compressed ${files[i]} \n----------------`);
-                successful_compressions++;
+                console.log(`> successfully compressed ${files[i]} \n----------------`);
+                successfulCompressions++;
             }
             catch(e) {
                 console.error(`An error occurred during compression of file "${files[i]}"`, e)
             }
         }
-        console.log(`successfully compressed ${successful_compressions} files.`)
+        console.log(`successfully compressed ${successfulCompressions} files.`)
     }
     catch(e) {
         console.error("Woah, something happened!", e);
