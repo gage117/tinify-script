fbbe6255fc06a53b11e494950a5d07c327d9e822
Gage Eide <gageeide@gmail.com>
2021-03-11 16:48:52 -0600

    Added ability to insert resize argument that can take a multiplier (.25 or 1.4 for example) or a resolution by width x height (1920x1080)

--- index.js
+++ index.js
@@ -4,6 +4,34 @@ const path = require('path');
 require('dotenv').config();
 const tinify = require('tinify');
 const sizeOf = require('image-size');
+const { type } = require('os');
+
+// Declare resizeArg to be eiher the 3rd argument sent in, or .5 by default
+let resizeArg = process.argv[2] || '.5';
+// Check if resizeArg is a string with a 'x' in it, indicating the resize arg is formatted <width>x<height>
+if (resizeArg.includes('x') || resizeArg.includes('X')) {
+    // replace uppercase X with lowercase x to make the logic leaner 
+    if (resizeArg.includes('X')) resizeArg = resizeArg.replace('X', 'x');
+    try {
+        // Split resoution string at x and turns returned strings into numbers
+        const resolutionArray = resizeArg.split('x');
+        if (resolutionArray.length > 2) throw new Error("Too many arguments provided for resolution resize");
+        resolutionArray[0] = Number(resolutionArray[0]);
+        resolutionArray[1] = Number(resolutionArray[1]);
+        resizeArg = resolutionArray;
+    }
+    catch(e) {
+        console.error(e)
+        throw new Error("Resolution resize arg not formatted correctly. The correct format is <width>x<height> e.g.: 1920x1080 or a resolution multiplier e.g.: .25 or 1.4");
+    }
+} else {
+    try {
+        resizeArg = Number(resizeArg);
+    }
+    catch(e) {
+        throw new Error("Resolution resize arg not formatted correctly. The correct format is <width>x<height> e.g.: 1920x1080 or a resolution multiplier e.g.: .25 or 1.4");
+    }
+}
 
 tinify.key = process.env.API_KEY;
 
@@ -25,9 +53,18 @@ const compressedFolder = './compressed';
                 
                 // Get current image dimensions to maintain aspect ratio while resizing
                 let originalDimensions = sizeOf(path.join(uncompressedFolder, files[i]));
-                let targetDimensions = {
-                    width: Math.floor(originalDimensions.width/2),
-                    height: Math.floor(originalDimensions.height/2)
+                let targetDimensions;
+                // Check which format resizeArg is using and assign targetDimensions accordingly
+                if (typeof resizeArg === 'number') {
+                    targetDimensions = {
+                        width: Math.floor(originalDimensions.width*resizeArg),
+                        height: Math.floor(originalDimensions.height*resizeArg)
+                    }
+                } else {
+                    targetDimensions = {
+                        width: Math.floor(resizeArg[0]),
+                        height: Math.floor(resizeArg[1])
+                    }
                 }
                 console.log(`> image dimensions:`, originalDimensions.width, originalDimensions.height);
                 console.log(`> target dimensions:`, targetDimensions.width, targetDimensions.height)
